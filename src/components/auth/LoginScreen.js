c63317ec74c69a6a44160c731c000ee39672860c
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-02-28 18:31:56 -0500

    primer dispacher e instalar firebase

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+	const dispatch = useDispatch()
+
+	const [values, handleInputChange] = useForm({
+		email: 'redhunter@gmail.com',
+		password: '123456',
+	})
+	const { email, password } = values
+
+	const handleLogin = (e) => {
+		e.preventDefault()
+		dispatch(login('12sd5', 'Jhonny Quispe'))
+	}
+
 	return (
 		<>
 			<h3 className='auth__title'>Login</h3>
 
-			<form>
+			<form onSubmit={handleLogin}>
 				<input
 					type='text'
 					placeholder='Email'
 					name='email'
 					className='auth__input'
 					autoComplete='off'
+					value={email}
+					onChange={handleInputChange}
 				/>
 				<input
 					type='password'
@@ -20,6 +38,8 @@ export const LoginScreen = () => {
 					name='password'
 					className='auth__input'
 					autoComplete='off'
+					value={password}
+					onChange={handleInputChange}
 				/>
 				<button className='btn btn-primary btn-block' type='submit'>
 					Login
