64f1b0cf3cba1ff969e2a88011f1068b7ec25b08
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-05 18:56:40 -0500

    eliminamos una card

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { activeNote } from '../../actions/notes'
+import { activeNote, startDeleting } from '../../actions/notes'
 import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
@@ -9,7 +9,7 @@ export const NoteScreen = () => {
 
 	const { active: note } = useSelector((state) => state.notes)
 	const [values, handleInputChange, reset] = useForm(note)
-	const { body, title } = values
+	const { body, title, id } = values
 
 	const activeId = useRef(note.id)
 
@@ -23,6 +23,10 @@ export const NoteScreen = () => {
 	useEffect(() => {
 		dispatch(activeNote(values.id, { ...values }))
 	}, [values, dispatch])
+
+	const handleDelete = () => {
+		dispatch(startDeleting(id))
+	}
 
 	return (
 		<div className='notes__main-content'>
@@ -48,10 +52,14 @@ export const NoteScreen = () => {
 
 				{note.url && (
 					<div className='notes__image'>
-						<img src={note.url} alt='' />
+						<img src={note.url} alt={title} />
 					</div>
 				)}
 			</div>
+
+			<button className='btn btn-danger' onClick={handleDelete}>
+				Delete
+			</button>
 		</div>
 	)
 }
