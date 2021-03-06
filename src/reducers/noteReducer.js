875daa30c8154cc0979d5480ea814a0737c07f3d
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-05 19:08:38 -0500

    limpiamos las notas del reducer al hacer logout

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -36,6 +36,9 @@ export const notesReducer = (state = initialState, action) => {
 				notes: state.notes.filter((note) => note.id !== action.payload),
 			}
 
+		case types.notesLogoutCleaning:
+			return initialState
+
 		default:
 			return state
 	}
